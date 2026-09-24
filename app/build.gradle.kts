98e750f6743b3c4ed1f2f4bf9dcf93d62856de91
rhythmcreative <rhythmcreative@local>
2026-09-24 19:40:51 +0200

    chore: Bump version to 1.0.2 with official Google preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rhythmcreative.motionassist"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
